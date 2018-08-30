6c180d0c3c2015063d29de8332b01b97bcb0de7e
jessicle <wong.jessica0411@gmail.com>
2018-08-30 10:58:25 -0400

    added lab_01 folder and smaller.cpp

--- lab_01/smaller.cpp
+++ lab_01/smaller.cpp
@@ -1,5 +1,26 @@
-//
-// Created by jessicle on 8/30/18.
-//
+/*
+Author: Jessica Wong
+Course: CSCI-136
+Instructor: Maryash / Zamansky
+Assignment: Lab1A
+
+Here, briefly, at least in one or a few sentences
+describe what the program does.
+*/
 
 #include "smaller.h"
+#include <iostream>
+using namespace std;
+
+class smaller {
+
+    int user_input1;
+    int user_input2;
+
+    cout << "I will be telling you which of two integers you input is the smaller integer\nPlease input the first integer" << endl;
+
+    cin >> user_input1;
+
+    return 0;
+
+};
--- lab_01/smaller.h
+++ lab_01/smaller.h
@@ -1,6 +1,12 @@
-//
-// Created by jessicle on 8/30/18.
-//
+/*
+Author: Jessica Wong
+Course: CSCI-136
+Instructor: Maryash / Zamansky
+Assignment: Lab1A
+
+Here, briefly, at least in one or a few sentences
+describe what the program does.
+*/
 
 #ifndef PROJECT_SMALLER_H
 #define PROJECT_SMALLER_H
