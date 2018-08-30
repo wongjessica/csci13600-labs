65c4c550393e8aef0a7d63c385a1a03d308edc99
jessicle <wong.jessica0411@gmail.com>
2018-08-30 10:48:18 -0400

    added lab_01 folder and smaller.cpp

--- lab_01/smaller.cpp
+++ lab_01/smaller.cpp
@@ -2,3 +2,4 @@
 // Created by jessicle on 8/30/18.
 //
 
+#include "smaller.h"
---
+++ lab_01/smaller.h
@@ -0,0 +1,14 @@
+//
+// Created by jessicle on 8/30/18.
+//
+
+#ifndef PROJECT_SMALLER_H
+#define PROJECT_SMALLER_H
+
+
+class smaller {
+
+};
+
+
+#endif //PROJECT_SMALLER_H
