ee5b6bb9d3846d5594b81c420c2586e74ff4da81
Jessica Wong <39626651+wongjessica@users.noreply.github.com>
2018-09-02 22:24:29 -0400

    Create smaller3.cpp

---
+++ lab_01/smaller3.cpp
@@ -0,0 +1,49 @@
+/*
+    Author: Jessica Wong
+    Course: CSCI-136
+    Instructor: Maryash / Zamansky
+    Assignment: Lab1B
+
+    This code will determine the smaller of 3 integers, inputted by user
+    */
+
+    #include <iostream>
+    using namespace std;
+
+    int main() {
+
+        int user_input1; //declares user_input1 as an int
+        int user_input2; //declares user_input2 as an int
+        int user_input3; //declares user_input3 as an int
+
+        cout << "Enter the first number: "; //system print statement
+
+        cin >> user_input1; //allows the user to input an int
+
+        cout << "Enter the second number: "; //system print statement
+
+        cin >> user_input2; //allows the user to input an int
+
+        cout << "Enter the third number: "; //system print statement
+
+        cin >> user_input3; //allows the user to input an int
+
+        if(user_input1>user_input2 && user_input2>user_input3) {
+            cout << "The smaller of the three is " << user_input3;
+        }
+        else if(user_input2>user_input3 && user_input3>user_input1) {
+            cout << "The smaller of the three is " << user_input1;
+        }
+        else if(user_input3>user_input2 && user_input2>user_input1){
+            cout << "The smaller of the three is " << user_input1;
+        }
+        else if(user_input2>user_input1 && user_input1>user_input3){
+            cout << "The smaller of the three is " << user_input3;
+        }
+        else {
+            cout << "The smaller of the three is " << user_input2;
+        }
+
+        return 0;
+
+    }
