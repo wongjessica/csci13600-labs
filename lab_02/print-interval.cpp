0a30a5104124c16d5d493c2bd9e1af19d62baf7a
jessicle <wong.jessica0411@gmail.com>
2018-09-10 13:45:32 -0400

    added print-interval.cpp

---
+++ lab_02/print-interval.cpp
@@ -0,0 +1,30 @@
+/*
+Author: Jessica Wong
+Course: CSCI-136
+Instructor: Zamansky / Maryash
+Assignment: Lab2A
+
+The program print-interval.cpp allows the user to enter two integers. Then the numbers between the integers
+ will be printed out, ending at the largest integer less than U (the second number inputted)
+*/
+
+#include <iostream>
+
+int main() {
+
+    int L; //declares L as an int
+    int U; //declares U as an int
+
+    std::cout << "Please enter L: " << std::endl;
+    std::cin >> L;
+    std::cout << "Please enter U: " << std::endl;
+    std::cin >> U;
+
+    for(int i = L; i < U; i++) {
+        std::cout << i << " " << std::endl;
+    }
+
+
+
+    return 0; //to assure the code worked as it should've
+}
