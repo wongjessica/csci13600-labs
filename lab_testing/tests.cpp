5cfc3776e914db4214e7747d48fd40d0046d3196
Jessica Wong <39626651+wongjessica@users.noreply.github.com>
2018-09-21 01:53:00 -0400

    Update tests.cpp

--- lab_testing/tests.cpp
+++ lab_testing/tests.cpp
@@ -1,16 +1,17 @@
+#define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 #include "pig.h"
-#define CATCH CONFIG_MAIN
 
-TEST_CASE(“Consonant Cases”) {
-CHECK(piglatinify("coffee")=="offeecay");
+
+TEST_CASE("Constant Cases"){
+  CHECK(piglatinify("coffee")=="offeecay");
 }
 
-TEST_CASE(“Vowel Cases”) {
-CHECK(piglatinify("elephant")=="elephantay");
+TEST_CASE("Vowel Cases"){
+  CHECK(piglatinify("elephant")== "elephantay");
+}
+TEST_CASE("Bad Cases"){
+  CHECK(piglatinify("")== "invalid input");
 }
 
-TEST_CASE(“Bad Input”) {
-CHECK(piglatinify("")== "Invalid input");
-CHECK(piglatinify(2) == "Invalid input");
-}
+
