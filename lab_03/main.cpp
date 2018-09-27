6a966c381f2c74170754e6f523494f430c6a907f
Jessica Wong <39626651+wongjessica@users.noreply.github.com>
2018-09-27 05:36:17 -0400

    Update main.cpp

--- lab_03/main.cpp
+++ lab_03/main.cpp
@@ -1,1 +1,20 @@
 
+#include <iostream>
+#include <cstdlib>
+#include <climits>
+#include <fstream>
+#include <string>
+#include "east-storage.h"
+
+int main()
+{
+  string date;
+  std::cout<< "Enter date in MM/DD/YYYY" << std::endl;
+  std::cin >> date;
+  double storage;
+  storage = east_storage(date);
+  std::cout << storage << " billion gallons" << std::endl;
+  
+  
+  return 0;
+}
