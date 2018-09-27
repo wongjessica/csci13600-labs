b3fc278a117132c0d17a2d1b0cc28e1684332de9
Jessica Wong <39626651+wongjessica@users.noreply.github.com>
2018-09-27 12:14:38 -0400

    Update main.cpp

--- lab_03/main.cpp
+++ lab_03/main.cpp
@@ -5,6 +5,9 @@
 #include <fstream>
 #include <string>
 #include "east-storage.h"
+#include "minmax.h"
+#include "compare.h"
+#include "reverse.h"
 
 int main()
 {
@@ -20,6 +23,11 @@ int main()
   
   std::cout << "Minimum storage in east basin: " << minimum << " billion gallons" << std::endl;
   std::cout << "Maximum storage in east basin: " << maximum << " billion gallons" << std::endl;
+  
+  string fDate, sDate;
+  string comp = compare(fDate, sDate);
+  
+  std::cout << fDate << comp << std::endl;
   
   
   
