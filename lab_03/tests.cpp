ae6f09603ea47c7ce7a8a2b9ac61d67ce1599f57
Jessica Wong <39626651+wongjessica@users.noreply.github.com>
2018-09-27 01:20:13 -0400

    Create tests.cpp

---
+++ lab_03/tests.cpp
@@ -0,0 +1,14 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include "east-storage.h"
+
+
+TEST_CASE("Dates"){
+  CHECK(east_storage("05/20/2016")== 79.88);
+}
+
+TEST_CASE("Invalid"){
+  CHECK(east-storage("") == -1);
+  CHECK(east-storage(5) == -1);
+}
+
