cc44c6cc9ed48b657b24b3b253d4bb24eeb9d949
Jessica Wong <39626651+wongjessica@users.noreply.github.com>
2018-10-01 09:54:16 -0400

    Update checkerboard.cpp

--- lab_04/checkerboard.cpp
+++ lab_04/checkerboard.cpp
@@ -1,1 +1,21 @@
+/*
+Author: Jessica Wong
+Course: CSCI-136
+Instructor: Zamansky / Maryash
+Assignment: Lab 4B
+
+*/ 
+
+#include <iostream>
+
+string box(int width, int height) {
+    string outp = "* ";
+    for (int y = 1; y <= width; y++) {
+        for (int x = 1; x <= height; x++) {
+            if (
+            return outp;
+        }
+        //std::cout << "\n" << std::endl;
+    }
+}
 
