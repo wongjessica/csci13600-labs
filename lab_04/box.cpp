94fa740750cbdf4a65a7c029c4a46b3abe9984f0
Jessica Wong <39626651+wongjessica@users.noreply.github.com>
2018-09-30 21:27:39 -0400

    Add files via upload

---
+++ lab_04/box.cpp
@@ -0,0 +1,15 @@
+/*
+Author: Jessica Wong
+Course: CSCI-136
+Instructor: Zamansky / Maryash
+Assignment: Lab 4A
+
+box.cpp asks the user to input width and height and prints a solid rectangular box of the requested size using asterisks.
+*/
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
