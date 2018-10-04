f5e6dcd1d41248c537ac96e4424dbd081897dfae
Jessica Wong <39626651+wongjessica@users.noreply.github.com>
2018-10-04 01:10:16 -0400

    Update trapezoid.h

--- lab_04/trapezoid.h
+++ lab_04/trapezoid.h
@@ -1,7 +1,3 @@
-#include <iostream>
-#include <cstdlib>
-#include <climits>
-#include <fstream>
-#include <string>
+#pragma once
 
-string trapezoid(int width, int height);
+std::string trapezoid(int width, int height);
