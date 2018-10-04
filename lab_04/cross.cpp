76de6ed79be369a68af6888d5ca71c7bdf4f2325
Jessica Wong <39626651+wongjessica@users.noreply.github.com>
2018-10-04 03:06:58 -0400

    Update cross.cpp

--- lab_04/cross.cpp
+++ lab_04/cross.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include "cross.h"
 
+using std::string;
+
+string cross(int size) {
+	std::string ans = "";
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			if (j == i || j == (size - i - 1)) {
+				ans += "*";
+			}
+			else {
+				ans += " ";
+			}
+		}
+		ans += "\n";
+	}
+	return ans;
+}
