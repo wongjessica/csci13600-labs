06531105471591431f0e9e4f0351d62dbd1b7c12
Jessica Wong <39626651+wongjessica@users.noreply.github.com>
2018-10-04 03:05:21 -0400

    Update upper.cpp

--- lab_04/upper.cpp
+++ lab_04/upper.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+#include "upper.h"
 
+using std::string;
+
+string upper(int length) {
+	string ans = "";
+	for (int i = length; i > 0; i--) {
+		ans += line(i, "*");
+	}
+	return ans;
+}
