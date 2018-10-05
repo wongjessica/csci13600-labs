a00a29dc894b835a58f99ee73d13c6a0d7434632
Jessica Wong <39626651+wongjessica@users.noreply.github.com>
2018-10-05 02:29:25 -0400

    Update checkerboard.cpp

--- lab_04/checkerboard.cpp
+++ lab_04/checkerboard.cpp
@@ -4,6 +4,7 @@ Course: CSCI-136
 Instructor: Zamansky / Maryash
 Assignment: Lab 4B
 
+checkerboard.cpp prints a checkerboard with the width and height that the user inputs
 */ 
 
 #include <iostream>
@@ -14,14 +15,40 @@ using std::cin;
 using std::endl;
 using std::string;
 
-string box(int width, int height) {
-    string outp = "* ";
-    for (int y = 1; y <= width; y++) {
-        for (int x = 1; x <= height; x++) {
-            if (
-            return outp;
-        }
-        //std::cout << "\n" << std::endl;
-    }
-}
+string checkerboard(int width, int height)
+{
+    
+	string outp;
+    
+	for (int h = 0; h < height; h++) 
+    { 
+		for (int w = 0; w < width; w = w + 2)
+        { 
+			if (h % 2 == 0)
+            {
+				if (w == width - 1) 
+                { 
+                    outp = outp + "*"; 
+                }
+				else 
+                { 
+                    outp = outp +  "* ";
+                }
+			}
+			else
+            {
+				if (w == width - 1)
+                {
+                    outp += " "; 
+                }
+				else 
+                {
+                    outp += " *";
+                }
+			}
+		}
+		outp += "\n";
+	}
 
+	return outp;
+}
