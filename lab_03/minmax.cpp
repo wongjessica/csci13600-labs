bbe76741076a66d083763ccd72efb9a0e8635024
Jessica Wong <39626651+wongjessica@users.noreply.github.com>
2018-10-05 01:54:34 -0400

    Update minmax.cpp

--- lab_03/minmax.cpp
+++ lab_03/minmax.cpp
@@ -11,75 +11,51 @@ The program minmax.cpp outputs the minimum and maximum storage in East basin in 
 #include <climits>
 #include <fstream>
 #include <string>
-#include minmax.h
+#include "minmax.h"
 using namespace std;
 
-double min_storage() {
-	ifstream fin("Current_Reservoir_Levels.tsv"); //opens input file stream
-	if (fin.fail())
-	{
+double min_storage(){
+	
+	string date;
+	double min = 100.0;
+	double eastStorage, eastElevation, westStorage, westElevation;
+	ifstream fin("Current_Reservoir_Levels.tsv");
+	if (fin.fail()) {
 		cerr << "File cannot be opened for reasons." << endl;
-		exit(1); //exits if failed to open file
+		exit(1); // exit if failed to open the file
 	}
-
-	string junk, date, userDate;
-	double eastSt, eastEl, westSt, westEl, max, min;
-	getline(fin, junk);	//reads the first line and stores it in junk
-
-	max = 0.0;	//assigning a default value of zero to the variables
-	min = 0.0;
-	while (fin >> date >> eastSt >> eastEl >> westSt >> westEl) //reads the file line by line
-	{
-		fin.ignore(INT_MAX, '\n');	//skips to the end of the line
-		if (eastSt > max)	//if the eastSt value is greater than the max
-		{				//then the max becomes the eastSt value
-			max = eastSt;
-		}
-		if (min == 0.0)	//an if statement used only once to set the min value to eastSt
-		{
-			min = eastSt;
-		}
-		if (min > eastSt) //if the previous min value is greater than the current eastSt value
-		{				  //the min value becomes the current eastSt value
-			min = eastSt;
+	string line;
+	getline(fin, line);
+	while(fin >> date >> eastStorage >> eastElevation >> westStorage >> westElevation) {
+		if(eastStorage < min) {
+			min = eastStorage;
 		}
 	}
-  return min;
-	fin.close();	//closes file
-	cin.get();
+	fin.close();
+	return min;
 }
 
-double max_storage() {
-	ifstream fin("Current_Reservoir_Levels.tsv"); //opens input file stream
-	if (fin.fail())
-	{
+double max_storage(){
+	
+	string date;
+	double max = 0.0;
+	double eastStorage, eastElevation, westStorage, westElevation;
+	ifstream fin("Current_Reservoir_Levels.tsv");
+	if (fin.fail()) {
 		cerr << "File cannot be opened for reasons." << endl;
-		exit(1); //exits if failed to open file
+		exit(1); // exit if failed to open the file
 	}
-
-	string junk, date, userDate;
-	double eastSt, eastEl, westSt, westEl, max, min;
-	getline(fin, junk);	//reads the first line and stores it in junk
-
-	max = 0.0;	//assigning a default value of zero to the variables
-	min = 0.0;
-	while (fin >> date >> eastSt >> eastEl >> westSt >> westEl) //reads the file line by line
-	{
-		fin.ignore(INT_MAX, '\n');	//skips to the end of the line
-		if (eastSt > max)	//if the eastSt value is greater than the max
-		{				//then the max becomes the eastSt value
-			max = eastSt;
+	string line;
+	getline(fin, line);	
+	while(fin >> date >> eastStorage >> eastElevation >> westStorage >> westElevation) {
+		if (max == 0.0) {
+			max = eastStorage;
+			continue;
 		}
-		if (min == 0.0)	//an if statement used only once to set the min value to eastSt
-		{
-			min = eastSt;
-		}
-		if (min > eastSt) //if the previous min value is greater than the current eastSt value
-		{				  //the min value becomes the current eastSt value
-			min = eastSt;
+		if(eastStorage > max) {
+			max = eastStorage;
 		}
 	}
-  return max;
-	fin.close();	//closes file
-	cin.get();
+	fin.close();
+	return max;
 }
