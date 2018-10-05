3c31ab646d50baa1f6e216a2248bd3d12e3f6bf8
Jessica Wong <39626651+wongjessica@users.noreply.github.com>
2018-10-05 02:52:43 -0400

    Update trapezoid.cpp

--- lab_04/trapezoid.cpp
+++ lab_04/trapezoid.cpp
@@ -1,3 +1,13 @@
+/*
+Author: Jessica Wong
+Course: CSCI-136
+Instructor: Zamansky 
+Assignment: Lab 4F
+trapezoid.cpp returns as a triangle representing an upside-down trapezoid of given width and height. 
+However, if the input height is impossibly large for the given width, then the program should report, Impossible shape!
+
+*/ 
+
 #include <iostream>
 #include "trapezoid.h"
 
@@ -7,17 +17,17 @@ using std::endl;
 using std::string;
 
 string trapezoid(int width, int height){
-	string ans = "";
-	if (w/2 < h) {
-		ans = "Impossible shape\n";
+	string outp = "";
+	if (width/2 < height) {
+		outp = "Impossible shape!\n";
 	}
 	else {
-		for (int i = 0; i < h; i++) {
-			int stars = w - (2 * i);
-			ans += line(i, " ").substr(0, i);
-			ans += line(stars, "*").substr(0, stars);
-			ans += line(i, " ");
+		for (int i = 0; i < height; i++) {
+			int aster = width - (2 * i);
+			outp += redo(i, " ").substr(0, i);
+			outp += redo(aster, "*").substr(0, aster);
+			outp += redo(i, " ");
 		}
 	}
-	return ans;
+	return outp;
 }
