bd28d6cffd2ef8263247ae75bb33c9655bdd04d7
Jessica Wong <39626651+wongjessica@users.noreply.github.com>
2018-10-05 02:45:38 -0400

    Create redo.h

---
+++ lab_04/redo.h
@@ -0,0 +1,7 @@
+#pragma once
+
+std::string redo1(int igr, std::string con);
+
+std::string redo2(int igr, std::string ast, std::string ris);
+
+std::string redo3(int igr, std::string ast, std::string ris);
