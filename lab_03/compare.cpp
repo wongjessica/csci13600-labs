e98b82d600d4ce8645076572ae3869200b097c35
Jessica Wong <39626651+wongjessica@users.noreply.github.com>
2018-10-05 02:01:40 -0400

    Update compare.cpp

--- lab_03/compare.cpp
+++ lab_03/compare.cpp
@@ -11,51 +11,36 @@ The program compare.cpp asks the user to input two dates and reports which basin
 #include <climits>
 #include <fstream>
 #include <string>
-#include compare.h
+#include "compare.h"
 using namespace std;
 
 string compare(string date1, string date2)
 {
-    ifstream fin("Current_Reservoir_Levels.tsv"); //opens input file stream
-    if (fin.fail())
-    {
-        cerr << "File cannot be opened for reasons." << endl;
-        exit(1); //exits if failed to open file
-    }
-
-    string junk, date, date1, date2;
+	string rtn = "";
+	string date;
     double eastSt, eastEl, westSt, westEl;
-    getline(fin, junk);    //reads the first line and stores it in junk
-
-    bool destination = false;
-    string str;
+	ifstream fin("Current_Reservoir_Levels.tsv"); //opens input file stream
+	if (fin.fail()) {
+		cerr << "File cannot be opened for reasons." << endl;
+		exit(1); // exit if failed to open the file
+	}
+	string junk;
+	getline(fin, junk); //reads the first line and stores it in junk
     while (fin >> date >> eastSt >> eastEl >> westSt >> westEl)
-    {
-        if (date1 == date && !destination)
-        {
-        destination = true;
-        }
-        if (destination)
-        {
-            if(eastEl > westEl) {
-                str.append(date);
-                str.append("East\n");
-            }
-            else if(westEl > eastEl) {
-                str.append(date);
-                str.append"West\n");
-            }
-            else {
-                str.append(date);
-                str.append("Equal\n");
-            }
-        }
-        if (date2 == date && destination)
-        {
-        break;
-        }
-    }
-
-    fin.close();    //closes file
-    return str;
+		if (date >= date1 && date <= date2) {
+			rtn += date;
+			if (eastEl < westEl) {
+				rtn += " West";
+			}
+			if (eastEl > westEl) {
+				rtn += " East";
+			}
+			if (eastEl == westEl) {
+				rtn += " Equal";
+			}
+			rtn += "\n";
+		}
+	}
+	fin.close();
+	return rtn;
 }
