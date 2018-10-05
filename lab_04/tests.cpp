7542b2077ab0de61b03eb0816ecb7f6044326fad
Jessica Wong <39626651+wongjessica@users.noreply.github.com>
2018-10-05 03:16:10 -0400

    Update tests.cpp

--- lab_04/tests.cpp
+++ lab_04/tests.cpp
@@ -7,6 +7,7 @@
 #include "lower.h"
 #include "trapezoid.h"
 #include "checkerboard3x3.h"
+#include "redo.h"
 
 TEST_CASE("Base Cases"){
 	CHECK(box(0,0) == "");
@@ -18,13 +19,35 @@ TEST_CASE("Base Cases"){
 	CHECK(checkerboard3x3(0,0) == "");
 }
 
-TEST_CASE("Random Cases"){
-	CHECK(box(2,4) == "****\n****\n****\n****\n****\n****\n");
-	CHECK(checkerboard(3,3) == "* * *\n * * \n* * *\n * * \n* * *\n");
-	CHECK(cross(6) == "*     *\n *   *\n  * *\n   *\n  * *\n *   *\n*     *\n");
+TEST_CASE("Valid Cases"){
+	CHECK(box(7,4) == "*******\n
+			   *******\n
+			   *******\n
+			   *******\n");
+	      
+	CHECK(checkerboard(11,6) == "
+	      			    * * * * * *\n
+ 				     * * * * * \n
+				    * * * * * *\n
+				     * * * * *\n
+				    * * * * * *\n
+ 				     * * * * *\n");
+	      
 	CHECK(cross(8) == "*    *\n *  *\n  **\n  **\n *  *\n*    *\n");
-	CHECK(lower(8) == "*\n**\n***\n****\n*****\n");
-	CHECK(upper(8) == "*****\n ****\n  ***\n   **\n    *\n");
+	      
+	CHECK(lower(6) == "
+*\n
+**\n
+***\n
+****\n
+*****\n
+******\n");
+	CHECK(upper(5) ==  "
+*****\n
+ ****\n
+  ***\n
+   **\n
+    *\n");
 	CHECK(trapezoid(9,3) == "********\n ******\n  ****\n");
 	CHECK(trapezoid(3,8) == "Impossible shape!\n");
 	CHECK(checkerboard3x3(7,7) == "***   ***\n***   ***\n***   ***\n   ***   \n   ***   \n   ***   \n***   ***\n***   ***\n***   ***\n");
