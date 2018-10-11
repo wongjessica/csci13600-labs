f7728a29d9d758c3289bb26149f5785782d285f3
Jessica Wong <39626651+wongjessica@users.noreply.github.com>
2018-10-11 03:07:21 -0400

    Update numbers.h

--- lab_05/numbers.h
+++ lab_05/numbers.h
@@ -1,1 +1,9 @@
+#pragma once
 
+bool isDivisibleBy(int n, int d);
+bool isPrime(int n);
+int nextPrime(int n);
+int countPrimes(int a, int b);
+bool isTwinPrime(int n);
+int nextTwinPrime(int n);
+int largestTwinPrime(int a, int b);
