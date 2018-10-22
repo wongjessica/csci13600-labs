a04f56cd9adc040c0b146a087f31514c2fefcf20
Jessica Wong <39626651+wongjessica@users.noreply.github.com>
2018-10-22 01:48:28 -0400

    Update test-ascii.cpp

--- lab_06/test-ascii.cpp
+++ lab_06/test-ascii.cpp
@@ -1,1 +1,27 @@
-lol
+/*
+	Author: Jessica Wong
+	Course: CSCI-133
+	Instructor: Zamansky
+	Assignment: Lab6A
+  
+  Asks the user to input a line of text (which may possibly include spaces). 
+  The program should report all characters from the input line together with their ASCII codes.
+*/
+
+#include <iostream>
+#include <string>
+#include "test-ascii.h"
+
+using std::string;
+
+string test_ascii(string s){
+	string ret = "";
+	for (int i = 0; i < s.length(); i++){
+		ret += s[i];
+		ret += " ";
+		int c = s[i];
+		ret += std::to_string(c);
+		ret += "\n";
+	}
+	return ret;
+}
