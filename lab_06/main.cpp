dbe587f3f616b0a3dabe6c80fcf1a3468b13dfdb
Jessica Wong <39626651+wongjessica@users.noreply.github.com>
2018-10-23 01:58:17 -0400

    Update main.cpp

--- lab_06/main.cpp
+++ lab_06/main.cpp
@@ -10,26 +10,26 @@ using std::string;
 
 int main(){	
 	string plaintext = "Hello, World!", keyword = "cake";
-	string cipher1 = "Olssv, Dvysk!", cipher2 = "Yeyoc, Ifryg!", keywordD = "random";
-	int shift1 = 7, shift2 = 6;
+	string cipher1 = "Rovvy, Gybvn!", cipher2 = "Jevpq, Wyvnd!", keywordD = "cake";
+	int shift1 = 9, shift2 = 8;
 	
 	cout << "plaintext: " << plaintext << endl;
 	cout << "shift1: " << shift1 << endl;
-	cout << "keyword: " << keyword << endl;
+	cout << "keyword1: " << keyword << endl;
 	cout << "cipher1: " << cipher1 << endl;
 	cout << "cipher2: " << cipher2 << endl;
-	cout << "keywordD: " << keywordD << endl;
+	cout << "keyword2: " << keywordD << endl;
 	cout << "shift2: " << shift2 << endl << endl;
 	
-	cout << "test_ascii(plaintext): \n" << test_ascii(plaintext) << endl;
+	cout << "test_ascii: \n" << test_ascii(plaintext) << endl;
 	
-	cout << "encryptCaesar(plaintext, shift1): " << encryptCaesar(plaintext, shift1) << endl;
+	cout << "encryptCaesar: " << encryptCaesar(plaintext, shift1) << endl;
 	
-	cout << "encryptVigenere(plaintext, keyword): " << encryptVigenere(plaintext, keyword) << endl;
+	cout << "encryptVigenere: " << encryptVigenere(plaintext, keyword) << endl;
 	
-	cout << "decryptCaesar(cipher1, shift2): " << decryptCaesar(cipher1, shift2) << endl;
+	cout << "decryptCaesar: " << decryptCaesar(cipher1, shift2) << endl;
 	
-	cout << "decryptVigenere(cipher2, keywordD): " << decryptVigenere(cipher2, keywordD) << endl;
+	cout << "decryptVigenere: " << decryptVigenere(cipher2, keywordD) << endl;
 	
 	return 0;
 }
