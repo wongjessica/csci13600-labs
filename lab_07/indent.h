bdfa685049d5fb37897bb6740adbc40ec2a687a5
Jessica Wong <39626651+wongjessica@users.noreply.github.com>
2018-11-05 09:05:30 -0500

    Update indent.h

--- lab_07/indent.h
+++ lab_07/indent.h
@@ -5,3 +5,5 @@ std::string removeLeadingSpaces(std::string line);
 int countChar(std::string line, char c);
 
 void print_unindent(std::string filename);
+
+void print_unindent2(std::string filename);
