ebbeb8e2913c8365d279f8343ee142c9a452a972
Jessica Wong <39626651+wongjessica@users.noreply.github.com>
2018-11-04 23:03:10 -0500

    Update indent.cpp

--- lab_07/indent.cpp
+++ lab_07/indent.cpp
@@ -8,66 +8,50 @@
 */
 
 #include <iostream>
-#include <fstream>
+#include <string>
 
-using std::cout;
-using std::cin;
-using std::endl;
 using std::string;
-using std::ifstream;
+using std::cin;
+using std::cout;
 
-string removeLeadingSpaces(string line){
-	if (line.length() == 0) return line;
-	for (int i = 0; i < line.length(); i++)
-		if (!isspace(line[i])) return line.substr(i);
-	return "";
-}
-
-int countChar(string line, char c){
-	int cnt = 0;
-	for (int i = 0; i < line.length(); i++)
-		if (line[i] == c) cnt++;
-	return cnt;
-}
-
-void print_unindent(string filename){
-	ifstream file;
-	file.open(filename);
-	if (!file.fail()){
-		int indents = 0;
-		string line;
-		while (getline(file, line)){
-			line = removeLeadingSpaces(line);
-			int left = countChar(line, '{');
-			int right = countChar(line, '}');
-
-			if (right && left){ //open and close on same line
-				if (line[0] == '}'){ //start with close
-					indents--;
-					for (int i = 0; i < indents; i++)
-						line = "\t" + line;
-					indents += left - right + 1;
-				}
-				else{
-					for (int i = 0; i < indents; i++)
-						line = "\t" + line;
-					indents += left - right;
-				}
-			}
-			else if (right){ //only close
-				indents -= right;
-				for (int i = 0; i < indents; i++)
-					line = "\t" + line;
-			}
-			else{ //only open
-				for (int i = 0; i < indents; i++)
-					line = "\t" + line;
-				indents += left;
-			}
-			cout << line << endl;
+int countChar(string line, char c) {
+	int count = 0;
+	for (int i = 0; i < line.length(); i++) {
+		if (line[i] == c) {
+			count++;
 		}
 	}
-	else{
-		cout << "File not found!" << endl;
+	return count;
+}
+
+string removeLeadingSpaces(string line) {
+	int index = 0;
+	while (isspace(line[index])) {
+		index++;
+		continue;
 	}
+	return line.substr(index);
+}
+
+int main() {
+	string line = "";
+	int numOpenBraces = 0;
+	int numClosedBraces = 0;
+	int numBlocks = 0;
+	while (getline(cin, line)) {
+		line = removeLeadingSpaces(line);
+		numOpenBraces += countChar(line, '{');
+		numClosedBraces += countChar(line, '}');
+		string multTabs = "";
+		if (line.at(0) == '}') {
+			numBlocks--;
+		}
+		for (int i = 0; i < numBlocks; i++) {
+			multTabs += "\t";
+		}
+		numBlocks = numOpenBraces - numClosedBraces;
+		cout << multTabs <<removeLeadingSpaces(line) << "\n";
+		line = "";
+	}
+	return 0;
 }
