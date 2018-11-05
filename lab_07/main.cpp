14f932067d8a45456f63513ab7c21c6cc3b9d59f
Jessica Wong <39626651+wongjessica@users.noreply.github.com>
2018-11-05 09:05:03 -0500

    Update main.cpp

--- lab_07/main.cpp
+++ lab_07/main.cpp
@@ -3,14 +3,17 @@
 
 using std::cout;
 using std::endl;
+using std::cin;
 using std::string;
 
 int main(){
+
+	cout << "Part A:" << endl << endl;
 	string filename = "bad-code.cpp";
-	print_unindent(filename);
-	cout << endl << "-----------------------------------" << endl << endl;
-	filename = "test-prog.cpp";
+	print_unindent2(filename);
+	cout << endl << "Part B:" << endl;
+	filename = "testprog.cpp";
 	print_unindent(filename);
 	
 	return 0;
-}
+}	
