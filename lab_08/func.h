7218c088cf75114eee3bcf8dd81f43aea37ee1e3
Jessica Wong <39626651+wongjessica@users.noreply.github.com>
2018-11-13 01:11:54 -0500

    Delete func.h

--- lab_08/func.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-const int MAX_H = 512;
-const int MAX_W = 512;
-
-void readImage(int image[MAX_H][MAX_W], int &height, int &width);
-void writeImage(std::string filename, int image[MAX_H][MAX_W], int height, int width);
