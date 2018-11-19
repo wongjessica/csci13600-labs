acbc7d7826402e27e78caea1206dcf23b294cfef
Jessica Wong <39626651+wongjessica@users.noreply.github.com>
2018-11-19 00:58:07 -0500

    Update poem.cpp

--- lab_09/poem.cpp
+++ lab_09/poem.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
 
+string * createAPoemDynamically() {
+    string *p = new string;
+    *p = "Roses are red, violets are blue";
+    return p;
+}
+
+int main() {
+    while(true) {
+        string *p;
+        p = createAPoemDynamically();
+
+	// cout << *p << endl;
+        // assume that the poem p is not needed at this point
+	delete p;
+    }
+}
