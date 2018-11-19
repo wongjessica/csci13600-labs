25bbbf050bdab3002f605b36d824fa95b2135102
Jessica Wong <39626651+wongjessica@users.noreply.github.com>
2018-11-19 00:57:27 -0500

    Update space3d.cpp

--- lab_09/space3d.cpp
+++ lab_09/space3d.cpp
@@ -1,66 +1,75 @@
-#include "funcs.h"
+#include <iostream>
+#include <cmath>
+#include "functions.h"
 
-double length(Coord3D *p) {
-	return sqrt(pow(p->x, 2) + pow(p->y, 2) + pow(p->z, 2));
+double length(Coord3D *p){
+  Coord3D location = *p;
+  double x = location.x;
+  double y = location.y;
+  double z = location.z;
+  double distance = sqrt((x*x) + (y*y) + (z*z));
+
+  return distance;
 }
 
-Coord3D* fartherFromOrigin(Coord3D *p1, Coord3D *p2) {
-	if (length(p1) > length(p2)) {
-		return p1;
-	}
-	else {
-		return p2;
-	}
+Coord3D * fartherFromOrigin(Coord3D *p1, Coord3D *p2){
+  Coord3D *ans;
+
+  if(length(p2) < length(p1)){
+    ans = p1;
+  }else{
+    ans = p2;
+  }
+  
+  return ans;
 }
 
-void move(Coord3D *pos, Coord3D *vel, double t) {
-	pos->x += vel->x * t;
-	pos->y += vel->y * t;
-	pos->z += vel->z * t;
+void move(Coord3D *ppos, Coord3D *pvel, double dt){
+  (*ppos).x = (*ppos).x + ((*pvel).x * dt);
+  (*ppos).y = (*ppos).y + ((*pvel).y * dt);
+  (*ppos).z = (*ppos).z + ((*pvel).z * dt);
 }
 
-Coord3D* createCoord3D(double x, double y, double z) {
-	Coord3D* ret = new Coord3D;
-	ret->x = x;
-	ret->y = y;
-	ret->z = z;
-	return ret;
+Coord3D* createCoord3D(double x, double y, double z){
+  Coord3D *result = new Coord3D;
+  *result = {x, y, z};
+  
+  return result;
 }
 
-void deleteCoord3D(Coord3D *p) {
-	delete p;
+void deleteCoord3D(Coord3D *p){
+  delete p;
+  
+  return;
 }
 
-Particle* createParticle(double x, double y, double z, double vx, double vy, double vz) {
-	Particle* ret = new Particle;
-	ret->x = x;
-	ret->y = y;
-	ret->z = z;
-	ret->vx = vx;
-	ret->vy = vy;
-	ret->vz = vz;
-	return ret;
+Particle* createParticle(double x, double y, double z, double vx, double vy, double vz){
+  Particle *result = new Particle;
+  *result = {{x,y,z},{vx,vy,vz}};
+
+  return result;
 }
 
-void setVelocity(Particle *p, double vx, double vy, double vz) {
-	p->vx = vx;
-	p->vy = vy;
-	p->vz = vz;
+void setVelocity(Particle *p, double vx, double vy, double vz){
+  (*p).pvel.x = vx;
+  (*p).pvel.y = vy;
+  (*p).pvel.z = vz;
 }
 
-Coord3D getPosition(Particle *p) {
-	Coord3D* ret = new Coord3D;
-	ret.x = p->x;
-	ret.y = p->y;
-	ret.z = p->z;
+Coord3D getPosition(Particle *p){
+  return (*p).ppos;
 }
 
-void move(Particle *p, double dt) {
-	p->x += p->vx * dt;
-	p->y += p->vy * dt;
-	p->z += p->vz * dt;
+void move(Particle *p, double dt){
+  (*p).ppos.x = (*p).ppos.x + ((*p).pvel.x * dt);
+  (*p).ppos.y = (*p).ppos.y + ((*p).pvel.y * dt);
+  (*p).ppos.z = (*p).ppos.z + ((*p).pvel.z * dt);
+
+  return;
 }
 
-void deleteParticle(Particle *p) {
-	delete p;
+void deleteParticle(Particle *p){
+  delete p;
+  
+  return;
 }
