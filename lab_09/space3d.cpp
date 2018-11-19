d4cc0da77aa784f104945928a73b55b926a5234e
Jessica Wong <39626651+wongjessica@users.noreply.github.com>
2018-11-19 08:40:45 -0500

    Update space3d.cpp

--- lab_09/space3d.cpp
+++ lab_09/space3d.cpp
@@ -1,75 +1,38 @@
 #include <iostream>
-#include <cmath>
-#include "functions.h"
+#include <math.h>
+#include "func.h"
 
+using std::cout;
+using std::cin;
+using std::endl;
+
+// distance from 3d coordinate to origin
 double length(Coord3D *p){
-  Coord3D location = *p;
-  double x = location.x;
-  double y = location.y;
-  double z = location.z;
-  double distance = sqrt((x*x) + (y*y) + (z*z));
-
-  return distance;
+	return sqrt(pow((*p).x, 2) + pow((*p).y, 2) + pow((*p).z, 2));
 }
 
+// returns which coordinate is further away from the origin
 Coord3D * fartherFromOrigin(Coord3D *p1, Coord3D *p2){
-  Coord3D *ans;
-
-  if(length(p2) < length(p1)){
-    ans = p1;
-  }else{
-    ans = p2;
-  }
-  
-  return ans;
+	return (length(p1) > length(p2)) ? p1 : p2;
 }
 
+// moves coordinate by given values
 void move(Coord3D *ppos, Coord3D *pvel, double dt){
-  (*ppos).x = (*ppos).x + ((*pvel).x * dt);
-  (*ppos).y = (*ppos).y + ((*pvel).y * dt);
-  (*ppos).z = (*ppos).z + ((*pvel).z * dt);
+	(*ppos).x += (*pvel).x * dt;
+	(*ppos).y += (*pvel).y * dt;
+	(*ppos).z += (*pvel).z * dt;
 }
 
+// creates new coordinate
 Coord3D* createCoord3D(double x, double y, double z){
-  Coord3D *result = new Coord3D;
-  *result = {x, y, z};
-  
-  return result;
+	Coord3D *ret = new Coord3D;
+	(*ret).x = x;
+	(*ret).y = y;
+	(*ret).z = z;
+	return ret;
 }
 
+// deletes coordinate
 void deleteCoord3D(Coord3D *p){
-  delete p;
-  
-  return;
-}
-
-Particle* createParticle(double x, double y, double z, double vx, double vy, double vz){
-  Particle *result = new Particle;
-  *result = {{x,y,z},{vx,vy,vz}};
-
-  return result;
-}
-
-void setVelocity(Particle *p, double vx, double vy, double vz){
-  (*p).pvel.x = vx;
-  (*p).pvel.y = vy;
-  (*p).pvel.z = vz;
-}
-
-Coord3D getPosition(Particle *p){
-  return (*p).ppos;
-}
-
-void move(Particle *p, double dt){
-  (*p).ppos.x = (*p).ppos.x + ((*p).pvel.x * dt);
-  (*p).ppos.y = (*p).ppos.y + ((*p).pvel.y * dt);
-  (*p).ppos.z = (*p).ppos.z + ((*p).pvel.z * dt);
-
-  return;
-}
-
-void deleteParticle(Particle *p){
-  delete p;
-  
-  return;
+	delete p;
 }
