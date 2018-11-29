cd2a5841c2ea9660a4dbba788467d38ea554c3dc
Jessica Wong <39626651+wongjessica@users.noreply.github.com>
2018-11-29 09:24:07 -0500

    Create fun_func.cpp

---
+++ lab_11/fun_func.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+bool isa(char c){return c == 'a';}
+bool isb(char c){return c == 'b';}
+bool isc(char c){return c == 'c';}
+bool isd(char c){return c == 'd';}
+bool ise(char c){return c == 'e';}
+bool isf(char c){return c == 'f';}
+bool isg(char c){return c == 'g';}
+bool ish(char c){return c == 'h';}
+bool isi(char c){return c == 'i';}
+bool isj(char c){return c == 'j';}
+bool isk(char c){return c == 'k';}
+bool isl(char c){return c == 'l';}
+bool ism(char c){return c == 'm';}
+bool isn(char c){return c == 'n';}
+bool iso(char c){return c == 'o';}
+bool isp(char c){return c == 'p';}
+bool isq(char c){return c == 'q';}
+bool isr(char c){return c == 'r';}
+bool iss(char c){return c == 's';}
+bool ist(char c){return c == 't';}
+bool isu(char c){return c == 'u';}
+bool isv(char c){return c == 'v';}
+bool isw(char c){return c == 'w';}
+bool isx(char c){return c == 'x';}
+bool isy(char c){return c == 'y';}
+bool isz(char c){return c == 'z';}
+bool isA(char c){return c == 'A';}
+bool isB(char c){return c == 'B';}
+bool isC(char c){return c == 'C';}
+bool isD(char c){return c == 'D';}
+bool isE(char c){return c == 'E';}
+bool isF(char c){return c == 'F';}
+bool isG(char c){return c == 'G';}
+bool isH(char c){return c == 'H';}
+bool isI(char c){return c == 'I';}
+bool isJ(char c){return c == 'J';}
+bool isK(char c){return c == 'K';}
+bool isL(char c){return c == 'L';}
+bool isM(char c){return c == 'M';}
+bool isN(char c){return c == 'N';}
+bool isO(char c){return c == 'O';}
+bool isP(char c){return c == 'P';}
+bool isQ(char c){return c == 'Q';}
+bool isR(char c){return c == 'R';}
+bool isS(char c){return c == 'S';}
+bool isT(char c){return c == 'T';}
+bool isU(char c){return c == 'U';}
+bool isV(char c){return c == 'V';}
+bool isW(char c){return c == 'W';}
+bool isX(char c){return c == 'X';}
+bool isY(char c){return c == 'Y';}
+bool isZ(char c){return c == 'Z';}
+bool is1(char c){return c == '1';}
+bool is2(char c){return c == '2';}
+bool is3(char c){return c == '3';}
+bool is4(char c){return c == '4';}
+bool is5(char c){return c == '5';}
+bool is6(char c){return c == '6';}
+bool is7(char c){return c == '7';}
+bool is8(char c){return c == '8';}
+bool is9(char c){return c == '9';}
+bool is0(char c){return c == '0';}
+bool islower(char c){return isa(c) || isb(c) || isc(c) || isd(c) || ise(c) || isf(c) || isg(c) || ish(c) || isi(c) || isj(c) || isk(c) || isl(c) || ism(c) || isn(c) || iso(c) || isp(c) || isq(c) || isr(c) || iss(c) || ist(c) || isu(c) || isv(c) || isw(c) || isx(c) || isy(c) || isz(c);}
+bool isupper(char c){return isA(c) || isB(c) || isC(c) || isD(c) || isE(c) || isF(c) || isG(c) || isH(c) || isI(c) || isJ(c) || isK(c) || isL(c) || isM(c) || isN(c) || isO(c) || isP(c) || isQ(c) || isR(c) || isS(c) || isT(c) || isU(c) || isV(c) || isW(c) || isX(c) || isY(c) || isZ(c);}
+bool isalpha(char c){return islower(c) || isupper(c);}
+bool isdigit(char c){return is0(c) || is1(c) || is2(c) || is3(c) || is4(c) || is5(c) || is6(c) || is7(c) || is8(c) || is9(c);}
+bool isalnum(char c){return isalpha(c) || isdigit(c);}
+
+int main(){
+	
+	
+	return 0;
+}
