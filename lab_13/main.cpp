2fcbf3710eeb0820b2d23b0da5218ecea44359a8
Jessica Wong <39626651+wongjessica@users.noreply.github.com>
2018-12-13 03:27:00 -0500

    Update main.cpp

--- lab_13/main.cpp
+++ lab_13/main.cpp
@@ -2,36 +2,36 @@
 #include "victor.hpp"
 
 int main(){
-	Victor<int> v(3, 100);
-	std::cout << "v = Victor(3, 100):   ";
+	Victor<int> v(4, 75);
+	std::cout << "v = Victor(4, 75):   ";
 	printVictor(v);
 
-	v.push_back(2);
-	std::cout << "v.push_back(2):       ";
+	v.push_back(6);
+	std::cout << "v.push_back(6):       ";
 	printVictor(v);
 
-	v[0] = 3;
-	std::cout << "v[0] = 3:             ";
+	v[0] = 5;
+	std::cout << "v[0] = 5:             ";
 	printVictor(v);
 
-	v[1] = 4;
-	std::cout << "v[1] = 4:             ";
+	v[1] = 7;
+	std::cout << "v[1] = 7:             ";
 	printVictor(v);
 
-	v[2] = 5;
-	std::cout << "v[2] = 5:             ";
+	v[2] = 11;
+	std::cout << "v[2] = 11:             ";
 	printVictor(v);
 
 	v.pop_back();
 	std::cout << "v.pop_back():         ";
 	printVictor(v);
 
-	v.insert(1, 500);
-	std::cout << "v.insert(1, 500):     ";
+	v.insert(5, 200);
+	std::cout << "v.insert(5, 200):     ";
 	printVictor(v);
 
-	v.erase(2);
-	std::cout << "v.erase(2):           ";
+	v.erase(5);
+	std::cout << "v.erase(5):           ";
 	printVictor(v);
 
 	Victor<int> v2;
@@ -54,16 +54,16 @@ int main(){
 	std::cout << "v2[2]:                ";
 	std::cout << v2[2] << std::endl;
 
-	v2.resize(10);
-	std::cout << "v2.resize(10)         ";
+	v2.resize(4);
+	std::cout << "v2.resize(4)         ";
 	printVictor(v2);
 
-	v2.resize(8);
-	std::cout << "v2.resize(8)          ";
+	v2.resize(9);
+	std::cout << "v2.resize(9)          ";
 	printVictor(v2);
 
-	v2.resize(10, 2);
-	std::cout << "v2.resize(10, 2)      ";
+	v2.resize(7, 1);
+	std::cout << "v2.resize(7, 1)      ";
 	printVictor(v2);
 
 	return 0;
